40fd8683a808cc8ee432e768b29588b2aac34d4a
SOVEREIGN <isimemeni@gmail.com>
2019-11-26 10:28:06 -0500

    added code to connection.js and orm.js

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,13 @@ DROP DATABASE IF EXISTS burgers_db;
 -- Creates the "todolist" database --
 CREATE DATABASE burgers_db;
 
-USE burgers_db
+USE burgers_db;
 
 CREATE TABLE burgers (
-    id AUTO_INCREMENT NOT NULL,
+    id INTEGER AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(25) NOT NULL,
     devoured BOOLEAN NOT NULL,  
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 
 
