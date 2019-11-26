3f02e75106da6e2ab6975742db4d0d8c6c53c112
SOVEREIGN <isimemeni@gmail.com>
2019-11-25 20:32:26 -0500

    added files and folders

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+-- Drops the todolist if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+-- Creates the "todolist" database --
+CREATE DATABASE burgers_db;
+
+USE burgers_db
+
+CREATE TABLE burgers (
+    id AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(25) NOT NULL,
+    devoured BOOLEAN NOT NULL,  
+    PRIMARY KEY (id),
+);
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bacon cheeseburger", 2);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cheeseburger", 10);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("triple stack", 5);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("regular", 7);
