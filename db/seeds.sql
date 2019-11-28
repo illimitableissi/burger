36d788826c53c33b4995ac56b033835030b39919
SOVEREIGN <isimemeni@gmail.com>
2019-11-28 16:11:44 -0500

    added handlebars functionality

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("bacon cheeseburger", true);
+INSERT INTO burgers (burger_name)
+VALUES ("bacon cheeseburger");
 
 INSERT INTO burgers (burger_name)
 VALUES ("cheeseburger");
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("triple stack", true);
+INSERT INTO burgers (burger_name)
+VALUES ("triple stack");
 
 INSERT INTO burgers (burger_name)
 VALUES ("regular");
